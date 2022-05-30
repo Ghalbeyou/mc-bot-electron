0c005355ad1ae10317e13e36c5becc6ed02bfb6c
Arash <97286379+Ghalbeyou@users.noreply.github.com>
2022-05-30 19:49:46 +0430

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# mc bot electron
+![](https://img.shields.io/github/license/ghalbeyou/mc-bot-electron)
+![](https://img.shields.io/discord/848844159419023410?color=green&label=Discord)
+![](https://img.shields.io/github/directory-file-count/ghalbeyou/mc-bot-electron)
+
+ An MC BOT attacker built with electron!
---
+++ index.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences:{
+        nodeIntegration: true,
+        contextIsolation: false,
+        devTools: true,
+      }
+    })
+  
+    win.loadFile('index.html')
+  }
+  app.whenReady().then(() => {
+    createWindow()
+  })
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
